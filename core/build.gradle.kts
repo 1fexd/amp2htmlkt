7da9a3299dc077c619233e2853e205110de07599
1fexd <PGFvy85nC@protonmail.com>
2024-06-25 12:44:06 +0200

    chore: BOM test

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,10 +2,10 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jsoup:jsoup:1.17.2")
+    api(platform("com.github.1fexd.super:platform:0.0.1"))
 
     testImplementation(kotlin("test"))
-    testImplementation("com.gitlab.grrfe.httpkt:core:13.0.0-alpha.62")
+    testImplementation("com.gitlab.grrfe.httpkt:core")
 }
 
 publishing.publications.create<MavenPublication>("maven") {
