df79e67f38c6176a4f450954887b813aee35a986
1fexd <PGFvy85nC@protonmail.com>
2024-06-25 12:58:29 +0200

    chore: BOM test

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 }
 
 dependencies {
-    api(platform("com.github.1fexd.super:platform:0.0.1"))
+    api(platform("com.github.1fexd:super:0.0.1"))
 
     testImplementation(kotlin("test"))
     testImplementation("com.gitlab.grrfe.httpkt:core")
